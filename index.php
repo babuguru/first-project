edc7b4a2281097ca5b1027b200640faf5e3d283c
babuguru <babu@verzdesign.com>
2017-02-08 15:54:34 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+<iframe src="http://localhost/guru/embed/" width="600" height="450" frameborder="0" allowfullscreen></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d15954.966854197475!2d103.87381445!3d1.3310654000000002!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2ssg!4v1486114054081" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+</body>
+</html>
